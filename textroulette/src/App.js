dfae7ac4ca2613ca0deaf2ee777aa625b34d703d
Default User <user@localhost.localdomain>
2019-02-20 20:22:43 -0500

    Version_3

--- textroulette/src/App.js
+++ textroulette/src/App.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
 import './App.css';
-import Loginscreen from './Loginscreen'
+
+import Chatroom from './Chatroom.js';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      loginPage:[],
-      uploadScreen:[]
-    }
-  }
-  componentWillMount(){
-    var loginPage = [];
-    loginPage.push(<Loginscreen parentContext={this}/>);
-    this.setState({loginPage:loginPage})
-  }
   render() {
     return (
-      <div className = "App">
-        {this.state.loginPage}
-        {this.state.uploadScreen}
+      <div className="App">
+        <Chatroom />
       </div>
     );
   }
 }
-
-const style = {
-  margin: 15,
-};
-
-if (response.data.code == 200) {
- console.log("Login successfull");
- var uploadScreen = [];
- uploadScreen.push(<UploadScreen appContext = {self.props.appContext}/>)
- self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
- }
 
 export default App;
